b8663580113612c8ccdafb851da69cd3e9be94a4
Chris Haegner <chris@stoyo.io>
2019-07-15 16:36:21 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :flats, only: [:index, :show]
+  get :dashboard, to: 'pages#dashboard', as: 'dashboard'
+  resources :transactions, only: [:new, :create]
+  resources :tasks, only: [:new, :create, :show, :edit, :update]
 end
