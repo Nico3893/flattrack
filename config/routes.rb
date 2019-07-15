9994b439e780ad308423202ffc5e039a8d482f18
Chris Haegner <chris@stoyo.io>
2019-07-15 16:50:21 +0200

    finally

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flats, only: [:index, :show] do
-    resources :tasks, only: [:new, :create]
+    resources :transactions, only: [:new, :create]
   end
-  resources :tasks, only: [ :show, :edit, :update]
+  resources :tasks, only: [ :new, :create, :show, :edit, :update]
   get :dashboard, to: 'pages#dashboard', as: 'dashboard'
-  resources :transactions, only: [:new, :create]
+
 end
