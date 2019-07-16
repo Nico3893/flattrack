2138006160d9a39649fe2e60603a734e6490f280
Nicolas Eberhardt <NicolasEberhardt93@gmail.com>
2019-07-16 08:51:33 +0200

    Updated routes, created Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flats, only: [:index, :show] do
     resources :transactions, only: [:new, :create]
   end
-  resources :tasks, only: [ :new, :create, :show, :edit, :update]
-  get :dashboard, to: 'pages#dashboard', as: 'dashboard'
+  resources :tasks, only: [ :index, :new, :create, :show, :edit, :update]
 
 end
