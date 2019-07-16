dec8900ce513b700446988509aba869b60017b7d
Nicolas Eberhardt <NicolasEberhardt93@gmail.com>
2019-07-16 12:16:16 +0200

    Seeds added, migration for user name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_165059) do
+ActiveRecord::Schema.define(version: 2019_07_16_100350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_165059) do
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.string "description"
-    t.string "status", null: false
+    t.string "status", default: "open", null: false
     t.bigint "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2019_07_15_165059) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "company_id"
+    t.string "name"
     t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
