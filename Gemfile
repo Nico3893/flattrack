fee53cb2e6a521104d888a7796407461dc34d03c
Nicolas Eberhardt <NicolasEberhardt93@gmail.com>
2019-07-17 09:26:26 +0200

    Graph broken status

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'carrierwave', '~> 1.2'
 gem 'pundit'
 gem 'geocoder'
 gem 'money-rails'
+gem "chartkick"
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+require("chartkick");
+require("chart.js");
+
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
