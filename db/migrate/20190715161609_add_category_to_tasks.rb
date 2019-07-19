73931c931234c832a813de88af0028df3e954020
Chris Haegner <chris@stoyo.io>
2019-07-19 12:38:28 +0200

    added inventory and inventory category

--- db/migrate/20190715161609_add_category_to_tasks.rb
+++ db/migrate/20190715161609_add_category_to_tasks.rb
@@ -1,7 +1,5 @@
 class AddCategoryToTasks < ActiveRecord::Migration[5.2]
   def change
     add_reference :tasks, :category, foreign_key: { to_table: :task_categories }
-    # add_reference :tasks, :category, index: true
-    # add_foreign_key :tasks, :task_categories, column: :category_id
   end
 end
